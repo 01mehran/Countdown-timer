f0aa6e2fdd58fc23e50e7490efd2f7aefad89da9
mehran <mehranmohamadi1311@gmail.com>
2025-12-20 15:32:55 +0330

    docs: install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <>Counter down</>;
+  return (
+    <div className="text-center md:bg-red-600 text-2xl bg-green-700 text-white p-4">
+      Counterdown timer with TailwindCss
+    </div>
+  );
 }
 
 export default App;
