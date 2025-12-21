89f1d852dd035fc1038d3560085a96646e5ba3ba
mehran <mehranmohamadi1311@gmail.com>
2025-12-21 14:25:14 +0330

    feat: add header

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,9 @@
+import Header from "./components/Header";
+
 export default function Home() {
   return (
     <div className="min-h-dvh bg-[url('./assets/background.png')] md:bg-bottom bg-cover bg-right bg-no-repeat text-slate-700 w-full flex items-center py-30 gap-16 flex-col space-y-0.5">
-      <div className="flex items-center flex-col space-y-2 font-serif">
-        <span className="text-2xl font-bold tracking-wide">
-          -- countdown to --
-        </span>
-        <h2 className="text-4xl font-extrabold">New Year</h2>
-      </div>
+      <Header />
 
       <div className="flex items-center justify-evenly w-full flex-wrap space-y-3 sm:space-y-0">
         <article className="timeBox">
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="flex items-center flex-col space-y-2 font-serif">
+      <span className="text-2xl font-bold tracking-wide">
+        -- countdown to --
+      </span>
+      <h2 className="text-4xl font-extrabold">New Year</h2>
+    </div>
+  );
+}
