77adcbae9e36638dccc59be250d6b7c8b62aa346
mehran <mehranmohamadi1311@gmail.com>
2025-12-31 15:00:34 +0330

    feat: implement toggle menu on mobile screen

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,36 @@
 import { Link } from "react-router-dom";
 import menu_icon from "../assets/menuicon.svg";
+import { useState } from "react";
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <nav className=" w-full h-16 flex items-center justify-between sm:px-14 px-4 shadow   ">
       <p className=" font-medium text-sm text-nowrap">Countdonw_Timer</p>
-      <ul className="sm:flex items-center space-x-6 hidden ">
+      <ul
+        className={`sm:flex items-center space-x-6 ${
+          isMenuOpen
+            ? " w-full gap-2 bg-linear-to-r bg-blue-600 via-blue-500 to-blue-400 fixed top-0 left-0 flex text-white flex-col py-4 "
+            : "hidden"
+        } `}
+      >
+        {isMenuOpen && (
+          <span
+            onClick={handleCloseMenu}
+            className="w-6  h-6 absolute right-0 text-xl cursor-pointer "
+          >
+            X
+          </span>
+        )}
         <li className="font-medium text-sm">
           <Link to="/">Gregorian New Year </Link>
         </li>
@@ -13,7 +38,10 @@ function NavBar() {
           <Link to="/Nowruz">Persian New Year (Nowruz)</Link>
         </li>
       </ul>
-      <div className="w-6 h-6 cursor-pointer sm:hidden block">
+      <div
+        className="w-6 h-6 cursor-pointer sm:hidden block"
+        onClick={handleToggleMenu}
+      >
         <img className="h-full w-full" src={menu_icon} alt="menu icon" />
       </div>
     </nav>
